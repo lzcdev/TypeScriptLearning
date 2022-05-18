4cadc67711d19a4b28425619aca4a27057a6c79b
lzcdev <lzc2134@gmail.com>
2022-05-18 20:33:22 +0800

    feat: add cheerio: 解析html

--- ts-spider/src/crowller.ts
+++ ts-spider/src/crowller.ts
@@ -1,17 +1,40 @@
 // ts -> .d.ts -> js
 
-import superagent from 'superagent'; 
+import superagent from 'superagent';
+import cheerio from 'cheerio';
+
+interface Course {
+  title: string,
+  count: number
+}
 
 class Crowller {
   private secret = 'secretKey';
-  private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
+  private url = `http://www.dell-lee.com/typescript/demo.html?secret=${
+    this.secret
+  }`;
 
-  private rawHtml = '';
+  getCourseInfo(html : string) {
+    const $ = cheerio.load(html);
+    const courseItmes = $('.course-item');
+    const courseInfos: Course[] = [];
+    courseItmes.map((index, element) => {
+      const descs = $(element).find('.course-desc');
+      const title = descs.eq(0).text();
+      const count = parseInt(descs.eq(1).text().split('：')[1])
+      // console.log(title, count);
+      courseInfos.push({title, count})
+    })
+    const result = {
+      time: new Date().getTime(),
+      data: courseInfos
+    }
+    console.log(result);
+  }
 
   async getRawHtml() {
     const result = await superagent.get(this.url)
-    this.rawHtml = result.text;
-    console.log(this.rawHtml);
+    this.getCourseInfo(result.text);
   }
 
   constructor() {
