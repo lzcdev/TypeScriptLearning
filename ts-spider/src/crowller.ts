4e1aadf42a35a36d3271f45cf674f7d7d64a95ff
lzcdev <lzc2134@gmail.com>
2022-05-19 23:23:29 +0800

    feat: 存储爬取的数据

--- ts-spider/src/crowller.ts
+++ ts-spider/src/crowller.ts
@@ -2,10 +2,21 @@
 
 import superagent from 'superagent';
 import cheerio from 'cheerio';
+import fs from 'fs';
+import path from 'path';
 
 interface Course {
   title: string,
   count: number
+}
+
+interface CourseResult {
+  time: number,
+  data: Course[];
+}
+
+interface Content {
+  [propName: number]: Course[];
 }
 
 class Crowller {
@@ -21,24 +32,42 @@ class Crowller {
     courseItmes.map((index, element) => {
       const descs = $(element).find('.course-desc');
       const title = descs.eq(0).text();
-      const count = parseInt(descs.eq(1).text().split('：')[1])
-      // console.log(title, count);
+      const count = parseInt(descs.eq(1).text().split('：')[1], 10)
       courseInfos.push({title, count})
     })
     const result = {
       time: new Date().getTime(),
       data: courseInfos
     }
-    console.log(result);
+    return result;
   }
 
   async getRawHtml() {
     const result = await superagent.get(this.url)
-    this.getCourseInfo(result.text);
+    return result.text;
+  }
+
+  generateJsonContext(courseInfo: CourseResult) {
+    const filePath = path.resolve(__dirname, '../data/course.json')
+    let fileContent: Content = {}
+    if (fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+    }
+    fileContent[courseInfo.time] = courseInfo.data;
+    return fileContent;
+  }
+
+
+  async initSpiderProcess() {
+    const html = await this.getRawHtml();
+    const courseInfo = this.getCourseInfo(html);
+    const fileContent = this.generateJsonContext(courseInfo)
+    const filePath = path.resolve(__dirname, '../data/course.json')
+    fs.writeFileSync(filePath, JSON.stringify(fileContent));
   }
 
   constructor() {
-    this.getRawHtml();
+    this.initSpiderProcess()
   }
 
 }
